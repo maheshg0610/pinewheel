4c5da082834cb52db3094d257e9d6b8d69874ab5
khushboo <khushboo.kumari@exponentialai.com>
2021-09-02 21:39:30 +0530

    removed /

--- src/app/shared/config/endpoint.config.ts
+++ src/app/shared/config/endpoint.config.ts
@@ -1,7 +1,7 @@
 let baseUrl = "http://ec2-3-13-20-24.us-east-2.compute.amazonaws.com:8081";
 
 export const endPoints = {
-    auth_user: baseUrl + '/api/login/',
+    auth_user: baseUrl + '/api/login',
     install_eseal: baseUrl + '/api/install-eseal',
     logout: baseUrl + '/logout',
     IDC_LIST: baseUrl + "/api/icd-list",
