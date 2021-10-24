894053e6635cb3946c3ae6e2025b5c8f0bdcbd07
khushboo <khushboo.kumari@exponentialai.com>
2021-10-24 21:25:34 +0530

    added header

--- src/app/components/auth/auth.interceptor.ts
+++ src/app/components/auth/auth.interceptor.ts
@@ -27,8 +27,9 @@ export class AuthInterceptor implements HttpInterceptor {
     } 
       this.authReq = req.clone({
         setHeaders: {
-          Authorization:'Bearer ' + this.currentUser.token, 
           "Access-Control-Allow-Origin": "*",
+          "accept": "text/plain",
+          Authorization:'Bearer ' + this.currentUser.token,
           "Content-Type": "application/json"
         }
       });
